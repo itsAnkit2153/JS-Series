83ffac0c7d48dc77b96012ff3fbe520df7f525f0
Ankit Vishwakarma <143541437+itsAnkit2153@users.noreply.github.com>
2025-07-29 19:38:06 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1455
+let accountEmail = "ankit@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+console.log(accountId)
+
+accountEmail = "kritika@gmail.com"
+accountPassword = "3234"
+accountCity = "Hydrabad"
+
+/*
+don't use "var" , try to use only "let"
+because of issue in block scope and functional scope (like if-else, for i.e for(){.... })
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
