eb74352bdcc2f6c5cf4dc73240ef796074966da5
Ankit Vishwakarma <143541437+itsAnkit2153@users.noreply.github.com>
2025-07-31 19:14:49 +0000

    object and api basics

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -48,4 +48,77 @@ console.log(obj3);//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' }
 const obj4 = Object.assign({}, obj1, obj2, obj5) // {} braces me saari value stored ho rahi hai and obj1, obj2 is source hai jise hume store karna hai in braces {} 
 //Note: agar {} braces nahi hoga to saari value obj1 me store ho jayega
 
+// OR
+
+const obj6 = {...obj1, ...obj2, ...obj5}// spread method
+
 console.log(obj4);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+console.log(obj6);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+//array ke andar kai saare object aur use kaise access karna hai
+
+const users = [
+    {
+        id: 1,
+        email:"ankit@gmail.com"
+    },
+    {
+        id: 2,
+        email:"two@.com"
+    },
+    {
+        id: 3,
+        email:"three@.com"
+    }
+]
+
+
+users[1].email
+
+console.log(Object.keys(tinderuser));//[ 'id', 'name', 'age', 'isLoggedIn' ]
+// yaha per hum 'tinderuser' ka saari keys print kara rahe hai from 'key:value' pair
+console.log(Object.values(tinderuser));//[ '123abc', 'Ankit', '22', false ]
+console.log(Object.entries(tinderuser));
+//entries of tinderuser
+/*[
+  [ 'id', '123abc' ],
+  [ 'name', 'Ankit' ],
+  [ 'age', '22' ],
+  [ 'isLoggedIn', false ]
+]
+  */
+
+console.log(tinderuser.hasOwnProperty("isLoggedIn")); // 'hasOwnProperty' check karte hai ki ye exist karta hai ki nahi in true/false 
+
+//******************************************************************** */
+
+const course = {
+    coursename: "javascript",
+    price: 999,
+    courseinstructor:"hitesh"
+}
+
+//course.courseinstructor
+//OR
+
+const {courseinstructor} = course // value extract karna hai kaha se 'course' se , kiska 'courseinstructor' ka
+console.log(courseinstructor);
+
+//OR
+
+const {courseinstructor:instructor} = course // agar 'courseinstructor' naam bada lagta hai to ish tarah se kar sakte ho
+console.log(instructor); // o/p => hitesh
+
+//how API file look like means JSON file
+
+// {
+//     "name": "Ankit",
+//     "age":22,
+//     "location":"prayagraj"
+// }
+//it is also contain in array where object declare
+[
+    {},
+    {},
+    {}
+]
