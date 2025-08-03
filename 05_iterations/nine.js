26f5530b953f1f09168a1b2394d2bdae6d4192b8
Ankit Vishwakarma <143541437+itsAnkit2153@users.noreply.github.com>
2025-08-03 07:35:36 +0000

    about reduce method

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -14,6 +14,30 @@ console.log(sumWithInitial);
 
 const myArray = [1, 2, 3]
 
-const myTotal = myArray.reduce(function(num) => ({
+// const myTotal = myArray.reduce(function(acc, currval){
+//   console.log(`accumulator value ${acc} and current value ${currval}`);
+//  return acc + currval
+// },0)// here initialize with zero which going to store 'acc' only first time after that sum of (acc+currvalue)
 
-}))
+//OR
+
+const myTotal = myArray.reduce((acc,currval) => acc+currval,0);
+console.log(myTotal);// 6
+
+const shoppingCart = [
+  {
+    onlineCourse: "java",
+    price: 1999
+  },
+  {
+    onlineCourse: "python",
+    price: 999
+  },
+  {
+    onlineCourse: "data science engg",
+    price: 2999
+  }
+]
+
+const paytoPrice = shoppingCart.reduce((acc,item) => (acc+item.price),0)
+console.log(paytoPrice);// total : 5997
